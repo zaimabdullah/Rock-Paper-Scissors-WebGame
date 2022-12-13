4da5940624de48d9962b856e568f8cdb9bfa9cf8
Husni Zaim <zcreative14@gmail.com>
2022-12-13 14:58:20 +0800

    Add core logic

--- app.js
+++ app.js
@@ -1,1 +1,80 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
+
+let gameIsRunning = false;
+
+// const start = function() {
+//   console.log('Game is starting...');
+// };
+
+// like this, we call function expression = function store in other variable
+// using function expression OVER function declaration(normal style) is, it will force you to declare function first before can use it
+
+// function without name = anonymous function & need ';'
+
+// const person = {
+//   name: 'Max', // call as property
+//   greet: function greet() {
+//     console.log('Hello there!');
+//   } // call as method @ function attached to an object
+// }
+// person.greet();
+
+// console.dir(startGame); function are object
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function (cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    cChoice === ROCK && pChoice === PAPER ||
+    cChoice === PAPER && pChoice === SCISSORS ||
+    cChoice === SCISSORS && pChoice === ROCK
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+};
+
+startGameBtn.addEventListener('click', function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is starting...');
+  const playerSelection = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerSelection);
+});
+
+//  use this style when the anonymous function only used here
