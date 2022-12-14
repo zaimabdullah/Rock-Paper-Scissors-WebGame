9f3fc9ee67525545afed0fd587489aeafaeacfa0
Husni Zaim <zcreative14@gmail.com>
2022-12-14 11:10:46 +0800

    Learn about rest parameter

--- app.js
+++ app.js
@@ -107,3 +107,25 @@ startGameBtn.addEventListener('click', () => {
 });
 
 //  use this style when the anonymous function only used here
+
+// not related to the game
+
+const sumUp = (a, b, ...numbers) => {
+  let sum = 0;
+  for (const num of numbers) {
+    sum += num;
+  }
+  return sum;
+};
+
+const subtractUp = function() {
+  let sum = 0;
+  for (const num of arguments) { // don't use this magic arguments which rest parameters style in ES5
+    sum += num;
+  }
+  return sum;
+}
+
+console.log(sumUp(1, 5, 10, -3, 6, 10)); // argument no more need in array when func declaration use ... @ rest parameters
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+console.log(subtractUp(1, 10, 15, 20));
